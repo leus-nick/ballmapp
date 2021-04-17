0c63bdd035b2ed102eeab77b2703d90f242db65e
leus-nick <nickaccreg@gmail.com>
2021-04-17 13:02:25 +0300

    initial commit

---
+++ src/store/store.ts
@@ -0,0 +1,13 @@
+import { configureStore, ThunkAction, Action, combineReducers } from '@reduxjs/toolkit';
+
+const rootReducer = combineReducers({
+  // Insert your reducers here
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
